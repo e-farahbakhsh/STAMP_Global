ed66ec6c44030d61d00b102c500b966bce41e76a
Ehsan Farahbakhsh <35557957+e-farahbakhsh@users.noreply.github.com>
2025-04-22 12:28:53 +1000

    update

---
+++ parameters.py
@@ -0,0 +1,14 @@
+parameters = {
+    
+    'plate_model_name': 'muller2022',
+    
+    'timespan': {
+    'min': 0,
+    'max': 540
+    },
+            
+    'outputs_dir': 'outputs',
+    
+    'subduction_data_filename': 'subduction_data.csv',
+        
+    }
